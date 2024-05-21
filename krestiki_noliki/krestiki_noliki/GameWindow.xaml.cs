6c718a08be34b8118c5ea7ed541e5c2b58e1230d
Tess_OK <88114924+SexDJ@users.noreply.github.com>
2024-05-21 17:50:50 +0300

    Add files via upload

---
+++ krestiki_noliki/krestiki_noliki/GameWindow.xaml.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace krestiki_noliki
+{
+    public partial class GameWindow : Window
+    {
+        private char[,] board = new char[3, 3];
+        private char currentPlayer = 'X';
+
+        public GameWindow()
+        {
+            InitializeComponent();
+            ResetGame();
+        }
+
+        private void ResetGame()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    board[i, j] = ' ';
+                }
+            }
+            currentPlayer = 'X';
+            UpdateBoard();
+        }
+
+        private void UpdateBoard()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Button button = (Button)FindName($"button{i}{j}");
+                    button.Content = board[i, j];
+                    button.Foreground = (board[i, j] == 'X') ? Brushes.Blue : Brushes.Red;
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            int row = int.Parse(button.Name[6].ToString());
+            int col = int.Parse(button.Name[7].ToString());
+
+            if (board[row, col] == ' ')
+            {
+                board[row, col] = currentPlayer;
+                UpdateBoard();
+                CheckWin();
+                currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+            }
+        }
+
+        private void CheckWin()
+        {
+            // Check rows
+            for (int i = 0; i < 3; i++)
+            {
+                if (board[i, 0] == board[i, 1] && board[i, 1] == board[i, 2] && board[i, 0] != ' ')
+                {
+                    MessageBox.Show($"{board[i, 0]} Выйграл!");
+                    ResetGame();
+                    return;
+                }
+            }
+
+            // Check columns
+            for (int i = 0; i < 3; i++)
+            {
+                if (board[0, i] == board[1, i] && board[1, i] == board[2, i] && board[0, i] != ' ')
+                {
+                    MessageBox.Show($"{board[0, i]} Выйграл!");
+                    ResetGame();
+                    return;
+                }
+            }
+
+            // Check diagonals
+            if (board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2] && board[0, 0] != ' ')
+            {
+                MessageBox.Show($"{board[0, 0]} Выйграл!");
+                ResetGame();
+                return;
+            }
+            if (board[0, 2] == board[1, 1] && board[1, 1] == board[2, 0] && board[0, 2] != ' ')
+            {
+                MessageBox.Show($"{board[0, 2]} Выйграл!");
+                ResetGame();
+                return;
+            }
+
+            // Check for a tie
+            bool isTie = true;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (board[i, j] == ' ')
+                    {
+                        isTie = false;
+                        break;
+                    }
+                }
+                if (!isTie)
+                    break;
+            }
+            if (isTie)
+            {
+                MessageBox.Show("Ничья!");
+                ResetGame();
+            }
+        }
+
+        private void ResetButton_Click(object sender, RoutedEventArgs e)
+        {
+            ResetGame();
+        }
+    }
+}
+
---
+++ krestiki_noliki/krestiki_noliki/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows;
+
+namespace krestiki_noliki
+{
+    public partial class MainWindow1 : Window
+    {
+        public MainWindow1()
+        {
+            InitializeComponent();
+        }
+
+        private void OptionsButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Игроки по очереди ставят на свободные клетки поля 3×3 знаки (один всегда крестики, другой всегда нолики). " +
+                "Первый, выстроивший в ряд 3 своих фигуры по вертикали, горизонтали или большой диагонали, выигрывает." +
+                " Если игроки заполнили все 9 ячеек и оказалось, что ни в одной вертикали, горизонтали или большой диагонали нет трёх одинаковых знаков, партия считается закончившейся в ничью." +
+                " Первый ход делает игрок, ставящий крестики. " +
+                "Обычно по завершении партии выигравшая сторона зачёркивает чертой свои три знака (нолика или крестика), составляющих сплошной ряд.");
+
+
+        }
+
+        private void StartGameButton_Click(object sender, RoutedEventArgs e)
+        {
+            GameWindow gw = new GameWindow();
+            gw.Show();
+            this.Close();
+        }
+
+        private void ProgressBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+
+        }
+    }
+}
